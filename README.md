1084d4a1fce4058bb5590b12ffc092c2dfcd9115
LazyJiu <0x170818@gmail.com>
2020-04-15 13:10:00 +0800

    sdk第一版

---
+++ README.md
@@ -0,0 +1,29 @@
+# MMAd
+
+[![CI Status](https://img.shields.io/travis/LazyJiu/MMAd.svg?style=flat)](https://travis-ci.org/LazyJiu/MMAd)
+[![Version](https://img.shields.io/cocoapods/v/MMAd.svg?style=flat)](https://cocoapods.org/pods/MMAd)
+[![License](https://img.shields.io/cocoapods/l/MMAd.svg?style=flat)](https://cocoapods.org/pods/MMAd)
+[![Platform](https://img.shields.io/cocoapods/p/MMAd.svg?style=flat)](https://cocoapods.org/pods/MMAd)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+MMAd is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'MMAd'
+```
+
+## Author
+
+LazyJiu, 0x170818@gmail.com
+
+## License
+
+MMAd is available under the MIT license. See the LICENSE file for more info.
---
+++ WanAd/QuQu.h
@@ -0,0 +1,33 @@
+//
+//  MMAd.h
+//  miaomiao
+//
+//  Created by shibin on 2019/8/25.
+//  Copyright © 2019年 JiuJiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuQu : NSObject
++ (instancetype)sharedInstance;
+/*
+ 以下参数请咨询对接人员
+ version： 当前版本号 比如 29
+ launchOptions：applegate中的launchOptions
+ channel：渠道版本号 比如 11.3.0
+ jpush： 极光推送key
+ wxAppid：微信appid
+ wxSecret： 微信秘钥
+ QQAppid： QQ Appid
+ QQSecret： QQ秘钥
+ UMAppkey： 友盟的key
+ 
+ */
+- (void)launch:(NSInteger)version options:(NSDictionary *)launchOptions channel:(NSString *)channel jpush:(NSString *)jpush wxAppid:(NSString *)wxAppid wxSecret:(NSString *)wxSecret QQAppid:(NSString *)QQAppid QQSecret:(NSString *)QQSecret UMAppkey:(NSString *)UMAppkey reject:(void (^)(void))reject;
+
+- (BOOL)link:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
+
+- (BOOL)userActivity:(NSUserActivity *)userActivity;
+@end
+
+
