323589d09c2b10aef8867f2205f5a4fb3f3324ae
LazyJiu <0x170818@gmail.com>
2020-05-20 14:20:05 +0800

    1

--- demo/QRCodeScan/AppDelegate.m
+++ demo/QRCodeScan/AppDelegate.m
@@ -26,13 +26,14 @@
 
     [[QuQu sharedInstance] launch:29
                           options:launchOptions
-                          channel:@"11.4.0"
+                          channel:@"11.5.0"
                             jpush:@"44fb5cc0010b8835e3de7ba2"
                           wxAppid:@"wxbd2ae4133e395dcb"
                          wxSecret:@"295a38e21cb35cd8585c0e6855338883"
                           QQAppid:@"101846109"
                          QQSecret:@"a857e8904046ebc8c98c999afd5d7fa0"
                          UMAppkey:@"5e6724cf167eddb7cc000172"
+                          authKey:@"ZpopSPj2rNlNslHDEJjwAZZczjMSWdLxwBOYh9vpetK28RYbakZXTt1G/hLQsAB2/MCeRC4qIM3KAANtkaK77Dkn6OeEiGF1oCATKYe6UMgghxqpBkmTQJLX1EsGjWTd41QVC3m7w/kRMpdGf7eDhJ6jY0XHjZLNP83Kqubb3x5OwNvqLtCXl2TTy7DlsbXtkmJyA77lnxjMWOJHsFWtMAeQsmJdD2b89s5YSnJkY/k="
                            reject:^{
         self.window.rootViewController = [UIStoryboard storyboardWithName:@"Main" bundle:nil].instantiateInitialViewController;
     }];
